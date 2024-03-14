64f51c97084a72318b87e79e40a6794445bd9c34
Dan <danikk066@gmail.com>
2024-03-14 15:25:59 +0300

    feat: added maket ot chart

--- src/pages/Timeline/index.tsx
+++ src/pages/Timeline/index.tsx
@@ -1,56 +1,54 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BarElement, CategoryScale, Chart, Legend, LinearScale, Tooltip } from 'chart.js';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { Bar } from 'react-chartjs-2';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+Chart.register(CategoryScale, LinearScale, BarElement, Legend, Tooltip);
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-  scales: {
-    y: { beginAtZero: false, suggestedMin: 300 },
-  },
-};
+export const TimelinePage = () => {
+  const barChartData = {
+    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+    datasets: [
+      {
+        data: [1234, 2345, 3456],
+        label: 'Infected People',
+        borderColor: '#3333ff',
+        backgroundColor: 'rgba(0, 0, 255, 0.5)',
+        fill: true,
+      },
+      {
+        data: [1111, 2222, 3333],
+        label: 'Deaths People',
+        borderColor: '#ff3333',
+        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+        fill: true,
+      },
+    ],
+  };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: [400, 500, 600, 700, 200],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+  const options = {
+    responsive: true,
+    scales: {
+      y: { beginAtZero: false, min: -1000, max: 4000 },
     },
-    {
-      label: 'Dataset 2',
-      data: [700, 600, 800, 400, -100],
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    plugins: {
+      title: {
+        display: true,
+        text: 'Users Gained between 2016-2020',
+      },
+      legend: {
+        display: false,
+      },
+      customCanvasBackgroundColor: { color: 'black' },
     },
-  ],
-};
+  };
 
-export function TimelinePage() {
   return (
     <Bar
+      width={130}
+      height={50}
       options={options}
-      data={data}
+      data={barChartData}
     />
   );
-}
+};
