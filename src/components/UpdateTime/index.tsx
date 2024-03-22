1d84e0dcdc02c47faf232907b0b5ea34406419e8
Dan <danikk066@gmail.com>
2024-03-22 18:15:00 +0300

    fix: fixed time update

--- src/components/UpdateTime/index.tsx
+++ src/components/UpdateTime/index.tsx
@@ -1,54 +1,30 @@
-import { Component } from 'react';
-
-import themeSwitcher from '@/store';
+import { useCurrencyRequest } from '@/hooks/useCurrencyRequest';
 
 import { Container, LastUpdate, LoadingText, StyledTimeImage } from './styled';
 
-interface State {
-  formattedUpdateTime: string;
-  loading: boolean;
-}
+export const UpdateTime = () => {
+  const { currency, loading } = useCurrencyRequest();
 
-export class UpdateTime extends Component<{}, State> {
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
-      formattedUpdateTime: '',
-      loading: true,
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.formatTime();
-    }, 2000);
-  }
-
-  formatTime = (): void => {
-    const date = new Date(themeSwitcher.currencyResponse.meta.last_updated_at);
+  const formatDate = () => {
+    const date = new Date(currency?.meta.last_updated_at as string);
     const hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
     const minutes = date.getMinutes().toString().padStart(2, '0');
 
     const ampm = date.getHours() >= 12 ? 'pm' : 'am';
 
-    this.setState({ formattedUpdateTime: `${hours}:${minutes}${ampm}`, loading: false });
+    return `${hours}:${minutes}${ampm}`;
   };
 
-  render() {
-    const { formattedUpdateTime, loading } = this.state;
-
-    return (
-      <Container>
-        {loading ? (
-          <LoadingText>Loading...</LoadingText>
-        ) : (
-          <>
-            <StyledTimeImage />
-            <LastUpdate>Last updated ad {formattedUpdateTime}</LastUpdate>
-          </>
-        )}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      {loading ? (
+        <LoadingText>Loading...</LoadingText>
+      ) : (
+        <>
+          <StyledTimeImage />
+          <LastUpdate>Last updated ad {formatDate()}</LastUpdate>
+        </>
+      )}
+    </Container>
+  );
+};
